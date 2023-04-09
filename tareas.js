67cedc703d82687b2d33ff4cb7dab26370d5a145
Stefania Veronica Bruera <59149@sistemas.frc.utn.edu.ar>
2023-04-09 10:58:14 -0300

    Creacion del modelo Tareas

---
+++ tareas.js
@@ -0,0 +1,18 @@
+import { Model, DataTypes } from 'sequelize'
+import sequelize from './db.js'
+
+const Tarea = sequelize.define('Tarea',
+
+  {
+    descripcion: { type: DataTypes.STRING },
+  },
+
+  {
+    sequelize,
+    modelName: 'tarea',
+    timestamps: false
+  }
+
+)
+
+export default Tarea
